86e4d49b52f359273bccc8fbabae54dcb493ae1d
Andy <andrew.rutherford.6@gmail.com>
2020-10-22 09:39:24 +0200

    client routing and LaunchItem component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Launches from './components/Launches';
+import Launch from './components/Launch';
 
 import rocket from './assets/rocket.svg';
 
@@ -12,17 +14,20 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='container'>
-        <header>
-          <img
-            src={rocket}
-            alt='Launch Monitor'
-            style={{ display: 'block', width: '60px', marginRight: '1rem' }}
-          />
-          <h1 className='display-4'>Launch Monitor</h1>
-        </header>
-        <Launches />
-      </div>
+      <Router>
+        <div className='container'>
+          <header>
+            <img
+              src={rocket}
+              alt='Launch Monitor'
+              style={{ display: 'block', width: '60px', marginRight: '1rem' }}
+            />
+            <h1 className='display-4'>Launch Monitor</h1>
+          </header>
+          <Route exact path='/' component={Launches} />
+          <Route exact path='/launch/:flight_number' component={Launch} />
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
--- client/src/components/LaunchItem.js
+++ client/src/components/LaunchItem.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const LaunchItem = ({ launch }) => {
+  const history = useHistory();
   const {
     flight_number,
     mission_name,
@@ -10,12 +12,26 @@ const LaunchItem = ({ launch }) => {
   return (
     <div className='card card-body mb-3'>
       <div className='row'>
-        <div className='col-md-9 '>
-          <h4>Mission: {mission_name}</h4>
+        <div className='col-md-9'>
+          <div className='d-flex align-items-center'>
+            <h4>Mission: {mission_name}</h4>
+            <span
+              className={`badge badge-pill badge-${
+                launch_success ? 'success' : 'danger'
+              } ml-2`}
+            >
+              {launch_success ? 'Success' : 'Failed'}
+            </span>
+          </div>
           <p>Date: {launch_date_local.slice(0, 10)}</p>
         </div>
         <div className='col-md-3 d-flex justify-content-center align-items-center'>
-          <button className='btn btn-primary'>Details</button>
+          <button
+            className='btn btn-primary'
+            onClick={() => history.push(`/launch/${flight_number}`)}
+          >
+            Details
+          </button>
         </div>
       </div>
     </div>
